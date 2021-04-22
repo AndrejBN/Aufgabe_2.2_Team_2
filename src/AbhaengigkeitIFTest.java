cd0c40ed3fa6b8c2572a268cd69e2606c0fed959
Urs Schöler <maegovannen88@gmail.com>
2021-04-22 13:17:58 +0200

    if applied this commit will add 3 simple tests.

--- src/AbhaengigkeitIFTest.java
+++ src/AbhaengigkeitIFTest.java
@@ -1,16 +1,30 @@
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AbhaengigkeitIFTest {
 
+    String[] A;
+    String[] B;
+    String[] C;
+
     @BeforeEach
     void setUp() {
+        A = new String[]{"A","B","C","D"};
+        B = new String[]{"C", "B", "D", "A"};
+        C = new String[]{"D", "D", "D"};
     }
 
-    @org.junit.jupiter.api.AfterEach
+
+    @AfterEach
     void tearDown() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    @DisplayName("Einfache Tests zu Beginn.")
     void isWellSorted() {
+        Assertions.assertTrue(isWellSorted(A));
+        Assertions.assertFalse(isWellSorted(B));
+        Assertions.assertFalse(isWellSorted(C));
     }
 }
